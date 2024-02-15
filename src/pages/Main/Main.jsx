a4f48cf3bf480c8784f302a6b4fc60b211a9c334
Samuel-Sales <cacabrbf@hotmail.com>
2024-02-14 22:18:54 -0300

    Adicionado Cards dinamicamente

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,6 @@
 import Card from "../../components/Card";
 import style from './Main.module.css';
+import posts from '../../BancoDeDados/db.json';
 
 export default function Main() {
     return (
@@ -7,14 +8,14 @@ export default function Main() {
             <main className="flex flex-col mb-20">
                 <h2 className="font-semibold text-3xl mb-28 mt-20 text-center">Artigos recomendados</h2>
                 <div className={style.container__cards}>  {/* grid gap-4 grid-cols-3 grid-rows-3 */}
-                    <div className={style.container__grid}>
-                        <Card />
-                        <Card />
-                        <Card />
-                        <Card />
-                        <Card />
-                        <Card />
-                    </div>
+                    <ul className={style.container__grid}>
+                        {posts.map((post) => (
+                            <li key={post.id}>
+                                <Card post={post} />
+                            </li>
+                        ))}
+                        
+                    </ul>
                 </div>
             </main>
 
